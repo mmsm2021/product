e370cecdac3da8fb27b2f8fbc0a3e5e9d2f838e8
Martin René Sørensen <git@sicet7.com>
2021-05-20 10:21:41 +0200

    Updated service lib and updated read endpoint.

--- src/app/Actions/Read.php
+++ src/app/Actions/Read.php
@@ -3,27 +3,52 @@
 namespace App\Actions;
 
 use App\Database\Repositories\ProductRepository;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Exception\HttpInternalServerErrorException;
 use \Throwable;
 
 class Read {
 
-    private $productRepository;
+    private ProductRepository $productRepository;
 
+    /**
+     * Read constructor.
+     * @param ProductRepository $productRepository
+     */
     public function __construct(ProductRepository $productRepository)
     {
         $this->productRepository = $productRepository;
     }
 
-    public function __invoke(Response $response, $productId)
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param string $productId
+     * @return Response
+     * @throws HttpInternalServerErrorException
+     */
+    public function __invoke(Request $request, Response $response, string $productId): Response
     {
         try {
-            $response->getBody()->write($this->productRepository->getById($productId));
+            $response->getBody()->write(
+                json_encode(
+                    $this->productRepository->getById($productId)->toArray(),
+                    JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT
+                )
+            );
+            $response->withBody()
             return $response;
         } catch (Throwable $e) {
-            return $response;
+            try {
+                $response->getBody()->write(json_encode([
+                    'error' => true,
+                    'message' => $e->getMessage(),
+                ], JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR));
+                return $response;
+            } catch (Throwable $exception) {
+                throw new HttpInternalServerErrorException($request, 'An Error occurred.', $exception);
+            }
         }
     }
 }
-
-?>
